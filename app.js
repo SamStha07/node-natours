7fb31c21efc86b1df7c145eed34b224972bb86b5
Sam <samshrestha85@gmail.com>
2020-08-26 23:41:56 +0545

    Mongoose/database connected

--- app.js
+++ app.js
@@ -7,35 +7,33 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // middleware betn req and res
-console.log(process.env.NODE_ENV);
+// console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'production') {
-    app.use(morgan('dev'));
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-    console.log('Hello from the middleware😂');
-    next();
+  console.log('Hello from the middleware😂');
+  next();
 });
 
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
+  req.requestTime = new Date().toISOString();
+  next();
 });
 
 // routes handlers - for homepage
 app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Hello from Server',
-        app: 'Natours',
-    });
+  res.status(200).json({
+    message: 'Hello from Server',
+    app: 'Natours',
+  });
 });
-
 
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
-dotenv.config({
-    path: './config.env'
-});
 const app = require('./app');
 
-// console.log(process.env);
+dotenv.config({
+  path: './config.env',
+});
+
+const DB = process.env.MONGO_URI;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('Mongoose connected...');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-    console.log(`Server running on port ${port}...`);
+  console.log(`Server running on port ${port}...`);
 });
