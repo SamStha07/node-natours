fb2f9a74529ee8e08af3fc3a780e4f91dd977986
Sam <samshrestha85@gmail.com>
2020-08-29 21:36:30 +0545

    Added sorting, field limit & pagination for queries

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,9 +1,12 @@
 const Tour = require('../model/tourModel');
+const {
+    Query
+} = require('mongoose');
 
 exports.getAllTours = async (req, res) => {
     try {
         // BUILD QUERY
-        // 1) Filtering
+        // 1A) Filtering
         const queryObj = {
             ...req.query
         };
@@ -13,7 +16,7 @@ exports.getAllTours = async (req, res) => {
         excludedFields.forEach(el => delete queryObj[el]); // it will delete all excludedFields from params
         //only shows this in req.query { difficulty: 'easy', ratingsAverage: '4.5' } these excepts the excluded fields
 
-        // 2) Advanced filtering
+        // 1B) Advanced filtering
         let queryStr = JSON.stringify(queryObj); //{ "difficulty": "easy", "ratingsAverage": "4.5" }
         // queryStr = '{ "averageCost": { "lt": "10000" }, "test": { "gt": "12345"} }';
         const regex = /\b(gt|gte|lt|lte)\b/g;
@@ -21,11 +24,42 @@ exports.getAllTours = async (req, res) => {
         // console.log(JSON.parse(queryStr));
         queryStr = JSON.parse(queryStr);
 
-        const query = Tour.find(queryStr); // if we use await in this query we willnot be able to use the filter query
+        let query = Tour.find(queryStr); // if we use await in this query we willnot be able to use the filter query
         // const query = Tour.find().where('duration).equals(5).where('difficulty').equals('easy')
 
+        // 2) Sorting
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            console.log(sortBy);
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort('-createdAt');
+        }
+
+        // 3) Field limiting
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields);
+        } else {
+            query = query.select('-__v'); // it will exclude from db not to show the user
+        }
+
+        // 4) Pagination
+        const page = req.query.page * 1;
+        const limit = req.query.limit * 1;
+        const skip = (page - 1) * limit; //skip the pages upto that limit
+
+        //page=2&limit=5
+        query = query.skip(skip).limit(limit);
+        if (req.query.page) {
+            const numTours = await Tour.countDocuments();
+            if (skip >= numTours) throw new Error('This page doesnot exist');
+        }
+
         // EXECUTE QUERY
+        // query.sort().select().skip().limit() query will change through all the queries gets called
         const tours = await query;
+
 
         // SEND RESPONSE
         res.status(200).json({
