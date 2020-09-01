7da5b6a443fecf38f95edca92094310603705641
Sam <samshrestha85@gmail.com>
2020-09-02 00:12:40 +0545

    Signing up users with token created

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,26 @@
+const jwt = require('jsonwebtoken');
 const User = require('../model/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 // Create new user
 exports.signup = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
 
-    res.status(201).json({
-        status: 'success',
-        data: {
-            user: newUser
-        }
-    });
+  // creates token of the current user signup
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+  res.status(201).json({
+    status: 'success',
+    token,
+    data: {
+      user: newUser,
+    },
+  });
 });
