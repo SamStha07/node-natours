2a2d57d863e1ce553d5c47d17978a80710d152ac
Sam <samshrestha85@gmail.com>
2020-09-01 16:20:11 +0545

    Added better error handler for global errors

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const GLobalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -32,11 +34,11 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 // middleware for unknown route to show error for all HTTPHeaders
+// Global error handling Middleware
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`,
-  });
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
+app.use(GLobalErrorHandler);
 
 module.exports = app;
