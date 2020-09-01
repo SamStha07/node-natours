557ad3bb31e37ccba871770d9ad2afee0e7c24e4
Sam <samshrestha85@gmail.com>
2020-09-01 15:51:22 +0545

    Added error handler for all unknown routes

--- app.js
+++ app.js
@@ -15,11 +15,6 @@ if (process.env.NODE_ENV === 'production') {
 app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware😂');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -35,5 +30,13 @@ app.get('/', (req, res) => {
 // ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// middleware for unknown route to show error for all HTTPHeaders
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
